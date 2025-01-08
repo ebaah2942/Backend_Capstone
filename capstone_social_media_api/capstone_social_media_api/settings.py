5791318c587405814fa60353383f5189d7dc286c
Enoch Baah <ebaah2942@gmail.com>
2025-01-08 09:26:33 +0000

    New corrections

--- capstone_social_media_api/capstone_social_media_api/settings.py
+++ capstone_social_media_api/capstone_social_media_api/settings.py
@@ -17,6 +17,7 @@ from api.middleware import DisableCSRFForAPI
 from dotenv import load_dotenv
 load_dotenv()
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,7 +32,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS').split(',')
 
 
 # Application definition
@@ -93,6 +94,11 @@ WSGI_APPLICATION = 'capstone_social_media_api.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+DATABASES = {
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL', default='postgres://localhost/newdb')
+    )
+}
 
 DATABASES = {
     'default': {
